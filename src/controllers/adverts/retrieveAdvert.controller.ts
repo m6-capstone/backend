c3261a47c0b26fe17d1347c00d1a859c57d3a300
AlexFlavio <alexflavio.dev@gmail.com>
2022-12-19 17:49:32 -0300

    feat: retrieveAdvert, feito controller e service relacionados a esta feat.

--- src/controllers/adverts/retrieveAdvert.controller.ts
+++ src/controllers/adverts/retrieveAdvert.controller.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
+import { retireveAdvertsService } from "../../services/adverts/retrieveAdvert.service";
 
 export const retrieveAdvertsController = async (
   req: Request,
   res: Response
-) => {};
+) => {
+  const id: string = req.params.userId;
+  const newAdverts = await retireveAdvertsService(id);
+  return res.status(201).json(newAdverts);
+};
