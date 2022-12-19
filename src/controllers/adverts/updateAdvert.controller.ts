eb9fe2f14cb0645f4d5ae188f005506748f93a36
AlexFlavio <alexflavio.dev@gmail.com>
2022-12-19 17:51:47 -0300

    feat: updateAdvert, feito controller e service relacionado a esta feat.

--- src/controllers/adverts/updateAdvert.controller.ts
+++ src/controllers/adverts/updateAdvert.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { IAdvertUpdateRequest } from "../../interfaces/adverts";
+import { updateAdvertService } from "../../services/adverts/updateAdvert.service";
 
 export const updateAdvertController = async (req: Request, res: Response) => {
-
+  const id: string = req.params.id;
+  const advertData: IAdvertUpdateRequest = req.body;
+  const adverts = await updateAdvertService(id, { ...advertData });
+  return res.status(200).json(adverts);
 };
---
+++ src/services/adverts/updateAdvert.service.ts
@@ -0,0 +1,48 @@
+import AppDataSource from "../../data-source";
+import { Adverts } from "../../entities/adverts";
+import AppError from "../../errors/AppError";
+import { IAdvertUpdateRequest } from "../../interfaces/adverts";
+
+export const updateAdvertService = async (
+  id: string,
+  {
+    advertsType,
+    title,
+    year,
+    mileage,
+    price,
+    description,
+    vehicleType,
+    coverImage,
+    galleryImages,
+    isActive,
+    isPublished,
+  }: IAdvertUpdateRequest
+) => {
+  const advertsRepository = AppDataSource.getRepository(Adverts);
+  const findAdvert = await advertsRepository.findOneBy({
+    id: id,
+  });
+
+  if (!findAdvert) {
+    throw new AppError("Advert not Found", 400);
+  }
+
+  await advertsRepository.update(id, {
+    advertsType: advertsType ? advertsType : findAdvert?.advertsType,
+    title: title ? title : findAdvert?.title,
+    year: year ? year : findAdvert?.year,
+    mileage: mileage ? mileage : findAdvert?.mileage,
+    price: price ? price : findAdvert?.price,
+    description: description ? description : findAdvert?.description,
+    vehicleType: vehicleType ? vehicleType : findAdvert?.vehicleType,
+    coverImage: coverImage ? coverImage : findAdvert?.coverImage,
+    galleryImages: galleryImages ? galleryImages : findAdvert?.galleryImages,
+    isActive: isActive ? isActive : findAdvert?.isActive,
+    isPublished: isPublished ? isPublished : findAdvert?.isPublished,
+  });
+
+  const advert = await advertsRepository.findOneBy({ id });
+
+  return advert;
+};
