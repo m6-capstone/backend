05e0aa8d5e96f6630659e6f995186e50dc35ee61
Daniel Josias <daniieljosiaslima@gmail.com>
2023-01-04 11:03:28 -0300

    feat: a rota de atualização de usuário está feita/acrescentei tbm a verificação de email da branch UserCreatesRoutes/o service retrieveAdverts tbm sofreu alteração na linha 8

--- src/services/users/createUsers.services.ts
+++ src/services/users/createUsers.services.ts
@@ -1,12 +1,33 @@
 import AppDataSource from "../../data-source";
-import { Users } from "../../entities/users";
+import AppError from "../../errors/AppError";
+import { User } from "../../entities/user";
 import { IUserCreateRequest } from "../../interfaces/users";
+import { hash } from 'bcryptjs'
 
-export const createUserService = async (UserData: IUserCreateRequest) => {
-  const userRepository = AppDataSource.getRepository(Users);
+export const createUserService = async ({
+  name,
+  email,
+  cpf,
+  cellphone,
+  birthdate,
+  description,
+  isSeller,
+  password
+}: IUserCreateRequest) => {
+  const userRepository = AppDataSource.getRepository(User);
+  const users = await userRepository.find()
 
-  const users = userRepository.create({...UserData});
-  await userRepository.save(users);
+  const emailAlreadyExists = users.find(users => users.email === email)
 
-  return users
+  if(emailAlreadyExists){
+    throw new AppError("Email already exists", 401)
+  }
+
+  const newPassword = await hash(password, 10)
+
+  const user = userRepository.create({name,email,cpf,cellphone,birthdate,description,isSeller,password: newPassword});
+  
+  await userRepository.save(user);
+
+  return user
 };
