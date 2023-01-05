c44d8a582da2a84326b2159fc57aef979fba2c1c
Daniel Josias <daniieljosiaslima@gmail.com>
2023-01-05 10:53:20 -0300

    feat: a rota de listar comentário está feita

--- src/controllers/comments/comments.controllers.ts
+++ src/controllers/comments/comments.controllers.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express"
 import { ICommentsCreateRequest } from "../../interfaces/comments";
 import { createCommentsService } from "../../services/comments/createComments.services";
-
+import { listCommentsService } from "../../services/comments/listComments.services";
 
 export const createCommentsController = async (req: Request, res: Response) => {
     //advertsId: req.params.id;
     //userId: req.users;
     const { text, userId, advertsId }: ICommentsCreateRequest = req.body;
     
-    const users = await createCommentsService({text, userId, advertsId});
-    return res.status(201).json(users);
+    const comments = await createCommentsService({text, userId, advertsId});
+    return res.status(201).json(comments);
+}
+
+export const listCommentsController = async (req: Request, res: Response) => {
+    const comments = await listCommentsService();
+    return res.status(200).json(comments);
 }
---
+++ src/services/comments/listComments.services.ts
@@ -0,0 +1,9 @@
+import AppDataSource from "../../data-source";
+import { Comments } from "../../entities/comments";
+
+export const listCommentsService = async ()=> {
+    const commentsRepository = AppDataSource.getRepository(Comments);
+    const comments = commentsRepository.find();
+
+    return comments
+}
