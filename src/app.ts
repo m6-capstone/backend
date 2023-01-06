859322cee3cf7d63bff3db280d4dbce3a4e3b2ba
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2023-01-06 10:26:26 -0300

    fix: cors e gitignore

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,13 @@ import { errorMiddleware } from "./middlewares/handleError.middleware";
 
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger/swagger_output.json");
@@ -15,19 +22,10 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(express.json());
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 appRoutes(app);
 
 app.use(errorMiddleware);
-
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
 
 export default app;
