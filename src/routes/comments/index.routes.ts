3375db3183a215bf589c02ee8033e5cce84f6e0c
Daniel Josias <daniieljosiaslima@gmail.com>
2023-01-06 15:47:56 -0300

    feat: a rota de deletar comentários está feita

--- src/routes/comments/index.routes.ts
+++ src/routes/comments/index.routes.ts
@@ -1,14 +1,12 @@
 import { Router } from "express";
-import { createCommentsController } from "../../controllers/comments/comments.controllers";
+import { createCommentsController, deleteCommentsController} from "../../controllers/comments/comments.controllers";
 import { listCommentsController } from "../../controllers/comments/comments.controllers";
 import ensureAuthTokenMiddleware from "../../middlewares/ensureAuthToken.middleware";
+
 const commentsRoutes = Router();
 
-commentsRoutes.post(
-  "/:id",
-  ensureAuthTokenMiddleware,
-  createCommentsController
-);
+commentsRoutes.post("/:id",ensureAuthTokenMiddleware,createCommentsController);
 commentsRoutes.get("/", listCommentsController);
+commentsRoutes.delete("/:id", ensureAuthTokenMiddleware, deleteCommentsController);
 
 export { commentsRoutes };
