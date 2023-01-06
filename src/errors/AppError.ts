62528caec6de785e72e7c6d73573f593af5204db
AlexFlavio <alexflavio.dev@gmail.com>
2023-01-06 10:18:50 -0300

    fix: removendo comentarios AppError

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,35 +1,3 @@
-// class AppError extends Error {
-//   statusCode: number;
-
-//   constructor(message: string, statusCode: number = 400) {
-//     super();
-//     this.message = message;
-//     this.statusCode = statusCode;
-//   }
-// }
-
-// export default AppError;
-
-// import { Response } from "express";
-
-// export class AppError extends Error {
-//   statusCode;
-
-//   constructor(message: number, statusCode: number) {
-//     super();
-//     this.statusCode = statusCode;
-//     this.message = message;
-//   }
-// }
-
-// export const handleError = (err: AppError, resp: Response) => {
-//   const { statusCode, message } = err;
-//   return resp.status(statusCode).json({
-//     status: "error",
-//     statusCode,
-//     message,
-//   });
-
 import { Response } from "express";
 
 export class AppError extends Error {
